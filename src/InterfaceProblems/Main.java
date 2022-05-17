32711222504c986003243ab5567d41e17e648cd9
Drew <drewbdoo@gmail.com>
2022-05-17 16:46:44 -0400

    more progress

--- src/InterfaceProblems/Main.java
+++ src/InterfaceProblems/Main.java
@@ -1,5 +1,10 @@
 package InterfaceProblems;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
 //        //Interface in a box Part 1: Packables
@@ -17,29 +22,58 @@ public class Main {
 //        System.out.println(cd1);
 //        System.out.println(cd2);
 //        System.out.println(cd3);
-          //Part 2: Box
-        Box box = new Box(10);
+//          //Part 2: Box
+//        Box box = new Box(10);
+//
+//        box.add(new Book("Fyodor Dostoevsky", "Crime and Punishment", 2)) ;
+//        box.add(new Book("Robert Martin", "Clean Code", 1));
+//        box.add(new Book("Kent Beck", "Test Driven Development", 0.7));
+//
+//        box.add(new CD("Pink Floyd", "Dark Side of the Moon", 1973));
+//        box.add(new CD("Wigwam", "Nuclear Nightclub", 1975));
+//        box.add(new CD("Rendezvous Park", "Closer to Being Here", 2012));
+//
+//        System.out.println(box);
+//        //Part 3: Box weight
+//        System.out.println(box.weight());
+//        //Part 4: A Box is a package too
+//        Box box1 = new Box(5);
+//        box1.add(box);
+//        System.out.println(box1);
+//        Box box2 = new Box(4);
+//        box2.add(box1);
+//        System.out.println(box2);
+//        Box box3 = new Box(20);
+//        box3.add(box1);
+//        System.out.println(box3);
+//        //List as a method parameter
+//        List<String> names = new ArrayList<>();
+//        names.add("First");
+//        names.add("Second");
+//        names.add("Third");
+//
+//        System.out.println(returnSize(names));
+        //Map as a method parameter
+        Map<String, String> names = new HashMap<>();
+        names.put("1", "first");
+        names.put("2", "second");
 
-        box.add(new Book("Fyodor Dostoevsky", "Crime and Punishment", 2)) ;
-        box.add(new Book("Robert Martin", "Clean Code", 1));
-        box.add(new Book("Kent Beck", "Test Driven Development", 0.7));
+        System.out.println(returnSize(names));
 
-        box.add(new CD("Pink Floyd", "Dark Side of the Moon", 1973));
-        box.add(new CD("Wigwam", "Nuclear Nightclub", 1975));
-        box.add(new CD("Rendezvous Park", "Closer to Being Here", 2012));
 
-        System.out.println(box);
-        //Part 3: Box weight
-        System.out.println(box.weight());
-        //Part 4: A Box is a package too
-        Box box1 = new Box(5);
-        box1.add(box);
-        System.out.println(box1);
-        Box box2 = new Box(4);
-        box2.add(box1);
-        System.out.println(box2);
-        Box box3 = new Box(20);
-        box3.add(box1);
-        System.out.println(box3);
+
+
     }
+
+    //Map as a method parameter method
+    private static int returnSize(Map<String, String> names) {
+        return names.size();
+    }
+
+
+//    //List a method parameter method
+//    private static int returnSize(List<String> names) {
+//        return names.size();
+//
+//    }
 }

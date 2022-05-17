68c625666db5a1cf2d631efa055a6722bf7819a3
Drew <drewbdoo@gmail.com>
2022-05-17 08:28:38 -0400

    init git repo

---
+++ src/Hash.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class Hash {
+    public static void main(String[] args) {
+
+//        HashMap<String, String> nickName = new HashMap<>();
+//        nickName.put("matthew", "matt");
+//        nickName.put("micheal", "mix");
+//        nickName.put("arthur", "artie");
+//        System.out.println(nickName.get("matthew"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
